204f99f5be1b10fd0bc73209a490bbf152dea736
jfolder <jfolder@users.noreply.github.com>
2014-07-26 20:42:14 -0400

    testing

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
+/*
 	var request = {tag: topic,
 		      pagesize: 10,
 		        period: 'all_time'};
-	
+*/
+
 	var result = $.ajax({
-		url: "http://api.stackexchange.com/2.2/tags/" + topic + "/top-answerers/all_time?site=stackoverflow",
-		data: request,
-		dataType: "jsonp",
+		url: "http://gdata.youtube.com/feeds/api/videos?alt=json&v=2&q=cars&safeSearch=none&time=all_time&uploader=partner",
+		dataType: "json",
 		type: "GET",
 		})
 	.done(function(result){
-
+		/*
 		var searchResults = showSearchResults(request.tag, result.items.length);
 		$('.search-results').html(searchResults);
 
@@ -18,9 +19,18 @@ $(document).ready(function() {
 			var topPerson = showAnswerers(item);
 			$('.results').append(topPerson);
 		});
+		*/
+		/* result.feed.entry[0].title.$t */
+
+		var link = result.feed.entry[0].link[0].href;
+
+		$("section iframe").attr("src", link);
 	})
 	.fail(function(jqXHR, error, errorThrown){
+		/*
 		var errorElem = showError(error);
 		$('.search-results').append(errorElem);
+		*/
+		alert("There was a problem");
 	});
 });
